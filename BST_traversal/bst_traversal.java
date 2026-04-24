8734be179f4dca1537b4f49e9f79682ac1766ccb
Ayushman Rai <hiayushman25@gmail.com>
2026-04-24 23:12:33 +0530

    Q107

---
+++ BST_traversal/bst_traversal.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) return ans;
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                level.add(node.val);
+
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+            }
+            ans.add(level);
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
